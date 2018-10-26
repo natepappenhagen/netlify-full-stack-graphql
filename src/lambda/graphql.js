b2348f5e5add06404f8161233f022ec0f9657cbc
Nate Pappenhagen <natepappenhagen@gmail.com>
2018-10-25 20:49:04 -0600

    added 1 resolver

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,21 +1,33 @@
 const { ApolloServer, gql } = require('apollo-server-lambda');
+import fetch from 'node-fetch';
 
 const typeDefs = gql`
   type Query {
-    hello: String
-    dogPhotoUrl: String
+    address(id: String!): String
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: (root, args, context) => 'Hello, world!',
-    dogPhotoUrl: (root, args, context) =>
-      'https://images.dog.ceo/breeds/pomeranian/n02112018_1090.jpg'
+    address: (parent, args) => {
+      const { id } = args
+      const apiCall = fetch(`https://api.etherscan.io/api?module=account&action=balance&address=${id}&tag=latest&apikey=${"8SIYM6JVM2H2248TJ1B1I3PDB4KX154KAJ"}`)
+                            .then(res => res.json()
+                            .then(data => data.result))
+      return apiCall
+    }
+    // dogPhotoUrl: (root, args, context) =>
+    //   'https://images.dog.ceo/breeds/pomeranian/n02112018_1090.jpg'
   }
 };
 
 export const handler = new ApolloServer({
+    cors: {
+    origin: '*',
+    credentials: true,
+  },
   typeDefs,
-  resolvers
+  resolvers,
+  introspection: true,
+  playground: true,
 }).createHandler();
